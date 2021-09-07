c8439a60e8a8c0222f05c9274a2f6cb88910b0e6
Owner <Owner@DESKTOP-3LV93VH>
2021-09-07 08:33:55 -0400

    Add project files.

---
+++ Tech Assigment 1 - Simple Console App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tech_Assigment_1___Simple_Console_App
+// ISM 4300 Demo Program - Submitted by Brian Best
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Professor! I hope this is to your liking!");
+        }
+    }
+}
